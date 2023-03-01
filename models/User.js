f9f1adc802c32d1fe854267f04beca00166e7f4c
jihyekwak <jihyekwak53@gmail.com>
2023-02-28 23:34:48 -0800

    Safely store password with bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -29,6 +32,26 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function(next) {
+    let user = this;
+
+    if(user.isModified('password')) {
+        // 비밀번호를 암호화시킴
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+    
+});
 
 const User = mongoose.model('User', userSchema);
 
