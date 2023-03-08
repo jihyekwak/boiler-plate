e264f9671d89b3d0140c8e00f41c221b0977fd84
jihyekwak <jihyekwak53@gmail.com>
2023-03-07 22:27:31 -0800

    Add form for register and login

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,8 +1,22 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const LandingPage = () => {
+
+    const navigate = useNavigate();
   
+    const logoutHandler = () => {
+        axios.get('/api/users/logout')
+        .then(response => {
+            if(response.data.success) {
+                navigate("/login")
+            } else {
+                alert('Failed to log out')
+            }
+        })
+    }
+
     return (
         <div style={{
             display: 'flex',
@@ -12,6 +26,8 @@ const LandingPage = () => {
             height: '100vh',
         }}>
         <h2>Landing Page</h2>
+
+        <button onClick={logoutHandler}>Log Out</button>
         </div>
     )
 };
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const RegisterPage = () => {
   
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const onNameHandler = (event) => {
+        setName(event.target.value);
+    }
+
+    const onEmailHandler = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const onConfirmPasswordHandler = (event) => {
+        setConfirmPassword(event.target.value);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        if (password !== confirmPassword) {
+            return alert('Password and confirm password do not match')
+        }
+
+        let body = {
+            name: name,
+            email: email,
+            password: password, 
+        }
+    }
+
     return (
         <div style={{
             display: 'flex',
@@ -10,7 +45,25 @@ const RegisterPage = () => {
             width: '100%',
             height: '100vh',
         }}>
-        <h2>Register Page</h2>
+            <form 
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column'
+                }}
+                onSubmit={onSubmit}
+            >
+                <label>Name</label>
+                <input name="name" type="text" value={name} onChange={onNameHandler} required/>
+                <label>Email</label>
+                <input name="email" type="email" value={email} onChange={onEmailHandler} required/>
+                <label>Password</label>
+                <input name="password" type="password" value={password} onChange={onPasswordHandler} required/>
+                <label>Confirm Password</label>
+                <input name="confirmPassword" type="password" value={confirmPassword} onChange={onConfirmPasswordHandler} required/>
+                
+                <br />
+                <button>Login</button>
+            </form>
         </div>
     )
 };
